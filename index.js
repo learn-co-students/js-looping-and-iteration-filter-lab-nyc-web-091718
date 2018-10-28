d9c5aae3fc05800119b459e742b64f99f69696e5
srscalia <shelby.scalia@gmail.com>
2018-10-28 16:52:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  let newDrivers = drivers.filter(function (person) {return name.toUpperCase() === person.toUpperCase()});
+  return newDrivers
+}
+
+function fuzzyMatch(drivers, letters) {
+  let length = letters.length;
+  let newDrivers = drivers.filter(function (person) {return letters === person.slice(0, length)});
+  return newDrivers;
+}
+
+
+function matchName(drivers, name) {
+  let newDrivers = drivers.filter(function (person) {return person.name === name});
+  return newDrivers;
+}
