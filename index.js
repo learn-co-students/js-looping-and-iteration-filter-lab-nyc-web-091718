cfd61f8032a9dc49d17627dfcc1383ddf9e0dd19
asc5025 <albert.s.chun@gmail.com>
2018-10-28 21:07:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(collection, driver) {
+  return collection.filter(function (driverName) {
+    return driverName.toLowerCase() === driver.toLowerCase()
+  })
+}
+
+function fuzzyMatch(collection, drivers) {
+  let driversLength = drivers.length;
+  return collection.filter(function (driverName) {
+    return driverName.slice(0, driversLength) === drivers;
+  });
+}
+
+function matchName(collection, name) {
+  return collection.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
