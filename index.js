f3b26b619d44ad792979d9a504f57143a2b6a007
TurtleFeeder <t.choong@yahoo.com>
2018-10-29 17:50:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, pName) {
+  return array.filter(function(name){return name.toLowerCase() === pName.toLowerCase()});
+}
+
+function fuzzyMatch(array, pName) {
+  return array.filter(
+    function(name) {
+      return name.toLowerCase().startsWith(pName.toLowerCase())
+    }
+  );
+}
+
+function matchName(array, pName) {
+  return array.filter(
+    function(element) {
+        return element.name.toLowerCase() === pName.toLowerCase()
+    });
+}
