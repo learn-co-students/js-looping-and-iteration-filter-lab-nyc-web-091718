b90a2aa444f54d1dfdbdcc1fc11cb23ef58c4289
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-29 08:34:35 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  let matchedDrivers = drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+  return matchedDrivers;
+}
+
+function fuzzyMatch(drivers, letters) {
+  let lengthOfLetters = letters.length;
+  let matchedDrivers = drivers.filter(function (driver) {
+    return driver.slice(0, lengthOfLetters) === letters
+  });
+  return matchedDrivers;
+}
+
+function matchName(drivers, name) {
+  let matchedDrivers = drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+  return matchedDrivers
+}
