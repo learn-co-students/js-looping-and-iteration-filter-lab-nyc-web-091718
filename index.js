fc3e42d7aa35adb767bc3929c51b37f7d0422693
github username <github email address>
2018-11-03 17:57:00 -0400

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function findMatching (drivers, string) {
 
 function fuzzyMatch(drivers, string) {
   return drivers.filter(function (driver) {
-    // return driver[0] + driver[1] === string;
     return driver.slice(0, string.length) === string;
   });
 }
